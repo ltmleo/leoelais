72834e2b7b6e85d4ea1d915729f1516704ad5e19
Leonardo Montero <leonardo.tmontero@telefonica.com>
2021-09-25 16:13:48 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app">
+        <h1>Leonardo e Lais</h1>
+        <Home/>
+    </div>
+</template>
+
+<script>
+import Home from "./pages/Home"
+
+export default {
+    components: { Home }
+}
+</script>
+
+<style>
+@font-face {
+    font-family: "RobotoMono";
+    src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+    font-family: "RobotoMono", "monospace", "sans-serif";
+}
+
+body {
+    margin: 0;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+    background: rgb(11,143,51);
+    background: linear-gradient(313deg, rgba(11,143,51,1) 0%, rgba(255,255,255,1) 44%);
+}
+</style>
