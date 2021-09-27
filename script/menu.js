1fdff668829acb98cbf19b79a43ae9fbcff65ee5
Leonardo Montero <leonardo.tmontero@telefonica.com>
2021-09-27 12:37:57 -0300

    feat: change menu

--- script/menu.js
+++ script/menu.js
@@ -1,8 +1,6 @@
-window.onhashchange = function(e) {
-    const oldURL = e.oldURL.split('#')[1]
-    const newURL = e.newURL.split('#')[1]
-    const oldLink = document.querySelector(`.menu a[href="#${oldURL}"]`)
-    const newLink= document.querySelector(`.menu a[href="#${newURL}"]`)
-    oldLink && oldLink.classList.remove('select')
-    newLink && newLink.classList.add('select')
-}
+$(document).ready(function(){
+    $('a').click(function(){
+      $('a').removeClass("active");
+      $(this).addClass("active");
+  });
+  });
